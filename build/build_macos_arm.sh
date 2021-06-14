555e3eb0fe112e07bc552c0f1d5b48ceb8dec64f
Tim Beermann <beermann@betacloud-solutions.de>
2021-06-14 07:05:23 +0200

    updated my build-script

--- build/build_macos_arm.sh
+++ build/build_macos_arm.sh
@@ -2,10 +2,11 @@
 
 echo "### FETCHING UPDATES ###"
 git remote add upstream git@github.com:VSCodium/vscodium.git
-git fetch upstream
+git fetch upstream --all --tags
 git checkout master
 git rebase upstream/master master
-git push -f origin master
+git tag -a $(git tag -l | tail -n 1).darwin_arm64 -m "Added Darwin ARM64 binary for M1 Macs." -m "Compiled on my M1 Macbook Air in just about 5 minutes 🥳"
+git push -f origin master --tags
 
 echo "### COMPILING ###"
 rm -rf VSCode*
